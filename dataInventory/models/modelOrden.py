204ef85f5bf35d95391dbeb2b5db311f61c1dbc7
SebastianL2 <sebas16cely@gmail.com>
2024-03-04 09:18:07 -0500

    def

--- dataInventory/models/modelOrden.py
+++ dataInventory/models/modelOrden.py
@@ -1,6 +1,7 @@
 from django.db import models
-
+from .modelProduct import Product
 class Orden(models.Model):
     nombre_pedido = models.CharField(max_length=200)
     fecha_pedido = models.DateField()
+    ordenes=models.ManyToManyField(Product)
     created_at = models.DateTimeField(auto_now_add=True)
